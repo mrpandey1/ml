554394143b78c5b1c9ac9b4dee7ec818df86a0e9
mrpandey1 <pandeynishant12345@gmail.com>
2020-03-17 15:33:24 +0530

    changes to linear regression

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -10,9 +10,9 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder=LabelEncoder()
-X[:,3]=labelencoder.fit_transform(X[:,3])
+from sklearn.preprocessing  import LabelEncoder,OneHotEncoder
+labelEncoder=LabelEncoder()
+X[:,3]=labelEncoder.fit_transform(X[:,3])
 onehotencoder=OneHotEncoder(categorical_features=[3])
 X=onehotencoder.fit_transform(X).toarray()
 
@@ -27,6 +27,7 @@ regressor=LinearRegression()
 regressor.fit(X_train,y_train)
 
 y_pred=regressor.predict(X_test)
+
 
 
 import statsmodels.formula.api as sm
