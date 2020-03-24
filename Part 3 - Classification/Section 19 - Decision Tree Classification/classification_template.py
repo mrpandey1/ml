4954f5b8cbcce56a8a066f4ace5e865aceecb4c6
mrpandey1 <pandeynishant12345@gmail.com>
2020-03-24 19:17:35 +0530

    Added DecisiontreeClassifier

--- Part 3 - Classification/Section 19 - Decision Tree Classification/classification_template.py
+++ Part 3 - Classification/Section 19 - Decision Tree Classification/classification_template.py
@@ -1,4 +1,4 @@
-# Naive Bayes
+# Classification template
 
 # Importing the libraries
 import numpy as np
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -20,21 +20,15 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
-
+# Fitting classifier to the Training set
+# Create your classifier here
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
-
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -48,7 +42,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Training set)')
+plt.title('Classifier (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -66,7 +60,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Test set)')
+plt.title('Classifier (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
--- Part 3 - Classification/Section 19 - Decision Tree Classification/decision_tree_classification.py
+++ Part 3 - Classification/Section 19 - Decision Tree Classification/decision_tree_classification.py
@@ -1,40 +1,32 @@
-# Naive Bayes
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
 
-# Splitting the dataset into the Training set and Test set
+dataset=pd.read_csv('Social_Network_Ads.csv')
+X=dataset.iloc[:,[2,3]].values
+y=dataset.iloc[:,4].values
+
+
+#split train and set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0)
 
-# Feature Scaling
+#scaling data
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.fit_transform(X_test) 
 
-# Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
+from sklearn.tree import DecisionTreeClassifier
+classifier=DecisionTreeClassifier()
+classifier.fit(X_train,y_train)
 
+y_pred=classifier.predict(X_test)
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-
-# Making the Confusion Matrix
+#confusion  matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
-
+cm=confusion_matrix(y_pred,y_test)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -48,11 +40,13 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Training set)')
+plt.title('Decision Tree Classification (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
+
+
 
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
@@ -66,8 +60,10 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Test set)')
+plt.title('Decision Tree Classification (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
+
+
