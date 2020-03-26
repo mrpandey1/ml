13932b279efd78a97bcd277be6f2bc99e9f9d339
mrpandey1 <pandeynishant12345@gmail.com>
2020-03-26 19:05:26 +0530

    Added random_forest_classifier

--- Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.py
+++ Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.py
@@ -1,32 +1,36 @@
+# Random Forest Classification
+
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
 
-dataset=pd.read_csv('Social_Network_Ads.csv')
-X=dataset.iloc[:,[2,3]].values
-y=dataset.iloc[:,4].values
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-
-#split train and set
-from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0)
-
-#scaling data
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
-sc_X=StandardScaler()
-X_train=sc_X.fit_transform(X_train)
-X_test=sc_X.fit_transform(X_test) 
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
-from sklearn.tree import DecisionTreeClassifier
-classifier=DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(X_train,y_train)
+# Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
 
-y_pred=classifier.predict(X_test)
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
 
-#confusion  matrix
+# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cm=confusion_matrix(y_pred,y_test)
+cm = confusion_matrix(y_test, y_pred)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -40,13 +44,11 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Decision Tree Classification (Training set)')
+plt.title('Random Forest Classification (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
 
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
@@ -60,10 +62,8 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Decision Tree Classification (Test set)')
+plt.title('Random Forest Classification (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
