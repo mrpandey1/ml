d79cdfddaf3468a52581687b1002446060241ee7
mrpandey1 <pandeynishant12345@gmail.com>
2020-04-21 09:31:22 +0530

    some changes in nlp

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -4,44 +4,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
-
-# Cleaning the texts
 import re
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import GaussianNB
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
 corpus = []
-for i in range(0, 1000):
-    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
-    review = review.lower()
-    review = review.split()
-    ps = PorterStemmer()
-    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-    review = ' '.join(review)
-    corpus.append(review)
+def text_preprocessor(dataset):
+    for i in range(0, 1000):
+        review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+        review = review.lower()
+        review = review.split()
+        ps = PorterStemmer()
+        review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+        review = ' '.join(review)
+        corpus.append(review)
+    return corpus
 
 # Creating the Bag of Words model
-from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)
-X = cv.fit_transform(corpus).toarray()
-y = dataset.iloc[:, 1].values
+def bag_of_words(corpus):
+    cv = CountVectorizer(max_features = 1500)
+    X = cv.fit_transform(corpus).toarray()
+    y = dataset.iloc[:, 1].values
+    return X,y
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+def splitting_data(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+    return X_train,X_test,y_train,y_test
+
 
 # Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+def fitting_model(X_train,X_test,y_train,y_test):
+    classifier = GaussianNB()
+    classifier.fit(X_train, y_train)
+    # Predicting the Test set results
+    y_pred = classifier.predict(X_test)
+    return y_pred
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+def confusion_mat(y_test, y_pred):
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, y_pred)
+    return cm
+
+def all_in_one():
+    
