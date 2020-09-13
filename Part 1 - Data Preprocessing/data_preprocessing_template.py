357a7fdd4904b5ed8940d5dc610fb9a467a36790
mrpandey1 <pandeynishant12345@gmail.com>
2020-09-13 20:22:03 +0530

    testing the ownership of project

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
 
 """
-#for missing values
+for missing values
 from sklearn.preprocessing import Imputer 
 imputer=Imputer(missing_values='NaN',strategy='mean',axis=0)
 imputer=imputer.fit(x[:,1:])
